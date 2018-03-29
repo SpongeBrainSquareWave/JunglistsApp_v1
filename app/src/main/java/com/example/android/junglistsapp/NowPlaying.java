fec9d3aa55060fd9cba2e6cbe003c4af84ac1d8d
Mateusz Hanclich <33466966+SpongeBrainSquareWave@users.noreply.github.com>
2018-03-29 22:19:09 +0100

    update

--- app/src/main/java/com/example/android/junglistsapp/NowPlaying.java
+++ app/src/main/java/com/example/android/junglistsapp/NowPlaying.java
@@ -1,7 +1,11 @@
 package com.example.android.junglistsapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 /**
  * Created by Dyzio on 25/03/2018.
@@ -9,10 +13,48 @@ import android.support.v7.app.AppCompatActivity;
 
 public class NowPlaying extends AppCompatActivity {
 
+    ImageView trackImageView;
+    TextView trackArtistTextView;
+    TextView trackTitleTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.now_playing);
+        Intent incoming = getIntent();
+
+        if (incoming == null)
+            return;
+
+        ImageView backButton = findViewById(R.id.back_button);
+        backButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent backIntent = new Intent(NowPlaying.this, TracksActivity.class);
+                    backIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+                    startActivity(backIntent);
+                }
+        });
+
+        ImageView menuButton = findViewById(R.id.menu_button);
+        menuButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent menuIntent = new Intent(NowPlaying.this, MainActivity.class);
+                menuIntent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+                startActivity(menuIntent);
+            }
+        });
+        trackImageView = findViewById(R.id.track_image_view);
+        trackArtistTextView = findViewById(R.id.artist_name_text_view);
+        trackTitleTextView= findViewById(R.id.track_title_text_view);
+
+
+        trackArtistTextView.setText(incoming.getStringExtra("ARTIST_NAME"));
+        trackTitleTextView.setText(incoming.getStringExtra("TRACK_TITLE"));
+        trackImageView.setImageResource(incoming.getIntExtra("TRACK_IMAGE", 0));
+
 
     }
 }
--- app/src/main/java/com/example/android/junglistsapp/TracksActivity.java
+++ app/src/main/java/com/example/android/junglistsapp/TracksActivity.java
@@ -6,6 +6,7 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -14,7 +15,7 @@ import java.util.ArrayList;
 /**
  * Created by Dyzio on 14/03/2018.
  */
-public class TracksActivity  extends AppCompatActivity {
+public class TracksActivity extends AppCompatActivity {
 
     private ListView listView;
 
@@ -30,6 +31,7 @@ public class TracksActivity  extends AppCompatActivity {
         listView = (ListView) findViewById(R.id.list);
 
         ArrayList<Track> tracks = new ArrayList<Track>();
+
         tracks.add(new Track("The Callin", "True Tactix", R.drawable.true_tactix_the_calling, R.mipmap.play_button_image));
         tracks.add(new Track("Steve", "bob", R.drawable.true_tactix_the_calling, R.mipmap.play_button_image));
         tracks.add(new Track("sadasd", "True Tactix", R.drawable.true_tactix_the_calling, R.mipmap.play_button_image));
@@ -48,15 +50,35 @@ public class TracksActivity  extends AppCompatActivity {
         tracks.add(new Track("The Callin", "True Tactix", R.drawable.true_tactix_the_calling, R.mipmap.play_button_image));
 
 
-
         mAdapter = new TrackAdapter(this, tracks);
 
 
         listView.setAdapter(mAdapter);
 
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Track track = (Track) listView.getItemAtPosition(position);
+                Intent nowPlaying = new Intent(TracksActivity.this, NowPlaying.class);
+                nowPlaying.putExtra("TRACK_TITLE", track.getmTrackTitle());
+                nowPlaying.putExtra("ARTIST_NAME", track.getmArtistName());
+                nowPlaying.putExtra("TRACK_IMAGE", track.getmImageDrawable());
+                startActivity(nowPlaying);
 
+            }
+        });
 
+        ImageView backButton = findViewById(R.id.back_button_tracklist);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent backIntent = new Intent(TracksActivity.this, MainActivity.class);
+                backIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+                startActivity(backIntent);
 
+            }
+
+        });
     }
 
 }
